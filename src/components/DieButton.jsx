dd5d3bf989460e1c5ed9d0cad0dc9b3450445fdd
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-10 17:21:53 +0200

    feat: add accessibility parameters and onClick function for testing

--- src/components/DieButton.jsx
+++ src/components/DieButton.jsx
@@ -1,6 +1,14 @@
 export default function DieButton({ number }) {
+  function handleClick(event) {
+    console.log(event.currentTarget.value);
+  }
   return (
-    <button className="w-14 h-14 justify-self-center rounded-md shadow-md font-semibold text-2xl bg-white">
+    <button
+      onClick={handleClick}
+      value={number}
+      aria-label={`Number ${number}`}
+      className="w-14 h-14 justify-self-center rounded-md shadow-md font-semibold text-2xl bg-white"
+    >
       {number}
     </button>
   );
