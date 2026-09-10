08c1f4c28465f8045a41c77addd93ac3801ef7ce
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-10 15:29:41 +0200

    feat: create state for unique number, pass number to a dice (all dices have same number)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import DiceButton from "./DiceButton";
 
 export default function Main() {
+  const [randomNumber, setRandomNumber] = useState(0);
+  function handleClick() {
+    setRandomNumber(Math.floor(Math.random() * 6));
+  }
+
   return (
     <main className="px-6 py-10 flex flex-col gap-8 items-center text-center text-xl  sm:m-auto sm:rounded-2xl sm:max-w-lg bg-neutral-100 ">
       <div className="flex flex-col items-center">
@@ -12,11 +18,13 @@ export default function Main() {
       </div>
       <section className="grid grid-cols-2 gap-y-4 gap-x-12 sm:gap-8 sm:grid-cols-5 ">
         {Array.from({ length: 10 }).map((_, index) => (
-          <DiceButton key={index + 1} number={index + 1} />
+          <DiceButton key={index + 1} number={randomNumber} />
         ))}
-        {}
       </section>
-      <button className="px-8 py-2 rounded-md shadow-md shadow-neutral-400 font-semibold text-white bg-indigo-600">
+      <button
+        onClick={handleClick}
+        className="px-8 py-2 rounded-md shadow-md shadow-neutral-400 font-semibold text-white bg-indigo-600"
+      >
         Roll
       </button>
     </main>
