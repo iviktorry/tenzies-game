c847d9f20f620da9a47b5bb80206a31dadac7556
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-10 14:50:21 +0200

    style: add responsive styles to App and Main structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,28 @@
 export default function Main() {
   return (
-    <div>
-      <h1>Tenzies</h1>
-      <p>
-        Roll until all dice are the same. Click each die to freeze it at its
-        current value between rolls.
-      </p>
-      <section>
+    <main className="px-8 py-8 flex flex-col gap-8 items-center text-center text-xl  sm:m-auto sm:rounded-2xl sm:max-w-xl bg-neutral-100 ">
+      <div className="flex flex-col items-center">
+        <h1 className="font-semibold text-4xl">Tenzies</h1>
+        <p className="sm:max-w-[80%]">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
+      <section className="grid grid-cols-2 gap-y-2 w-full sm:gap-y-6 sm:grid-cols-5 ">
         <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
       </section>
-      <button>Roll</button>
-    </div>
+      <button className="px-6 py-2 rounded-md text-white bg-indigo-600">
+        Roll
+      </button>
+    </main>
   );
 }
