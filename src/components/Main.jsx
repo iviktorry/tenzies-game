35087d0e288c0ac0bb2aeb6a372ca2ac028bc7ae
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-11 13:17:12 +0200

    chore: add confetti when user won

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import DieButton from "./DieButton";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function Main() {
   const [randomNumbers, setRandomNumbers] = useState(generateRandomNumbers());
@@ -31,16 +32,14 @@ export default function Main() {
     );
   }
 
-  if (
+  const gameWon =
     randomNumbers.every((item) => item.isSelected) &&
-    randomNumbers.every((item) => item.value === randomNumbers[0].value)
-  ) {
-    console.log("game won");
-  }
+    randomNumbers.every((item) => item.value === randomNumbers[0].value);
 
   return (
     <main className="px-6 py-10 flex flex-col gap-8 items-center text-center text-xl  sm:m-auto sm:rounded-2xl sm:max-w-lg bg-neutral-100 ">
       <div className="flex flex-col items-center">
+        {gameWon && <Confetti />}
         <h1 className="font-semibold text-4xl">Tenzies</h1>
         <p className="sm:max-w-[80%]">
           Roll until all dice are the same. Click each die to freeze it at its
@@ -58,10 +57,11 @@ export default function Main() {
         ))}
       </section>
       <button
+        // onClick={newGame ? rollDice : setRandomNumbers(generateRandomNumbers())}
         onClick={rollDice}
         className="px-12 py-3 rounded-md shadow-md shadow-neutral-400 font-semibold text-white bg-indigo-600"
       >
-        Roll
+        {gameWon ? "New game" : "Roll"}
       </button>
     </main>
   );
